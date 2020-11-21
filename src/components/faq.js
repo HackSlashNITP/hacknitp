7e3cd24485c0f6140432bfedb5420ff22d3a4567
Neeraj Patil <patilneeraj2212@gmail.com>
2020-11-21 15:59:49 +0530

    FaQ Section Completed

--- src/components/faq.js
+++ src/components/faq.js
@@ -1,9 +1,41 @@
-import React, { Component } from "react"
+import { faMinusCircle, faPlusCircle } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import React, { Component, useState } from "react"
 import { FAQ } from "../shared/faq-data"
-import { FaqWrapper, FaqSection, FaqHeading } from "./style"
+import {
+  FaqWrapper,
+  FaqSection,
+  FaqHeading,
+  ExpandTag,
+  FaqSolutionWrapper,
+} from "./style"
+import { Collapse } from "reactstrap"
 
 function RenderFaq({ item }) {
-  return <FaqWrapper key={item.id}>{item.question}</FaqWrapper>
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggle = () => setIsOpen(!isOpen)
+
+  const toggleIcon = isOpen => {
+    if (isOpen) {
+      return <FontAwesomeIcon icon={faMinusCircle} />
+    } else {
+      return <FontAwesomeIcon icon={faPlusCircle} />
+    }
+  }
+  return (
+    <div>
+      <FaqWrapper key={item.id} role="button" onClick={toggle}>
+        <span style={{ marginRight: 20, paddingRight: 20 }}>
+          {item.question}
+        </span>
+        <ExpandTag>{toggleIcon(isOpen)}</ExpandTag>
+      </FaqWrapper>
+      <Collapse isOpen={isOpen}>
+        <FaqSolutionWrapper>{item.solution}</FaqSolutionWrapper>
+      </Collapse>
+    </div>
+  )
 }
 
 class Faq extends Component {
@@ -25,11 +57,11 @@ class Faq extends Component {
 
     return (
       <FaqSection>
-        <FaqHeading className="row justify-content-center">
+        <h1 className="heading row justify-content-center">
           Some Question Asked by You
-        </FaqHeading>
+        </h1>
         <div className="row d-flex  justify-content-center">
-          <ul className="col-11 col-md-9 list-unstyled ">{faq}</ul>
+          <ul className="col-11 col-md-9 col-lg-7 list-unstyled ">{faq}</ul>
         </div>
       </FaqSection>
     )
--- src/components/style.js
+++ src/components/style.js
@@ -47,6 +47,7 @@ export const Sponsorcarddiv = styled.div`
 export const FaqWrapper = styled.div`
   background: #260404;
   border-radius: 25px;
+  position: relative;
   color: #fff;
   font-family: Noto Sans;
   font-style: normal;
@@ -58,16 +59,36 @@ export const FaqWrapper = styled.div`
 export const FaqSection = styled.section`
   background: #0f0000;
   padding: 10vh 0;
+
+  .heading {
+    color: #fff8f8;
+    padding: 0 20px;
+    font-family: Poppins;
+    font-style: normal;
+    font-weight: 300;
+    font-size: 55px;
+    line-height: 82px;
+    text-align: center;
+    margin-bottom: 10vh;
+  }
 `
 
-export const FaqHeading = styled.h1`
-  color: #fff8f8;
-  padding: 0 20px;
+export const ExpandTag = styled.span`
+  position: absolute;
+  right: 25px;
+`
+
+export const FaqSolutionWrapper = styled.div`
+  color: floralwhite;
+  background-color: #941010;
   font-family: Poppins;
   font-style: normal;
   font-weight: 300;
-  font-size: 55px;
-  line-height: 82px;
+  font-size: 24px;
+  border-radius: 25px;
+  margin: 20px 0;
+  padding: 20px 30px;
+  line-height: 40px;
+
   text-align: center;
-  margin-bottom: 10vh;
 `
