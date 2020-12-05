a0cece146691195421b5483c2f8d4bd0c8b581db
Asmit <ViAsmit>
2020-12-05 20:48:30 +0530

    Updated SponserList

--- src/shared/sponsorlist.js
+++ src/shared/sponsorlist.js
@@ -1,4 +1,9 @@
 const Sponsorlist = [
+
+  {
+    img: "images/sponsors/devfolio.png",
+    link: "https://devfolio.co/"
+  },
   {
     img:"https://image.flaticon.com/icons/png/512/25/25231.png",
     link:"https://github.com/"
@@ -11,52 +16,88 @@ const Sponsorlist = [
     img: "images/sponsors/axure.png",
     link: "https://www.axure.com/",
   },
-  { link: "https://myways.in/", img: "images/sponsors/myways.png" },
-
+  {
+    link: "https://myways.in/",
+    img: "images/sponsors/myways.png"
+  },
   {
     link: "https://www.picxele.com/index.html",
     img: "images/sponsors/picxele.jpg",
   },
-
-  { link: "https://www.taskade.com/", img: "images/sponsors/taskade.png" },
-
-  { link: "https://repl.it/", img: "images/sponsors/replit.png" },
-
+  {
+    img: "images/sponsors/matic.png",
+    link: "https://matic.network/"
+  },
+  {
+    img: "images/sponsors/fold.svg",
+    link: "https://fold.money/"
+  },
+  {
+    link: "https://www.taskade.com/",
+    img: "images/sponsors/taskade.png"
+  },
+  {
+    link: "https://repl.it/",
+    img: "images/sponsors/replit.png" 
+  },
   {
     link: "https://givemycertificate.com/index.php",
     img: "images/sponsors/givemycerti.png",
   },
+  {
+    link: "https://egghead.io/",
+    img: "images/sponsors/egghead.png"
+  },
 
-  { link: "https://egghead.io/", img: "images/sponsors/egghead.png" },
-
-  { link: "https://www.framer.com/", img: "images/sponsors/framer.jpg" },
-
-  { link: "https://1password.com/", img: "images/sponsors/1password.png" },
-
-  { link: "https://www.echoar.xyz/", img: "images/sponsors/echoAR.jpg" },
-
-  { link: "https://rosenfeldmedia.com/", img: "images/sponsors/rosenfeld.png" },
-
-  { link: "https://uaceit.com/", img: "images/sponsors/uaceit.png" },
-
+  {
+    link: "https://www.framer.com/",
+    img: "images/sponsors/framer.jpg"
+  },
+  {
+    link: "https://1password.com/",
+    img: "images/sponsors/1password.png"
+  },
+  {
+    link: "https://www.echoar.xyz/",
+    img: "images/sponsors/echoAR.jpg"
+  },
+  {
+    link: "https://rosenfeldmedia.com/",
+    img: "images/sponsors/rosenfeld.png"
+  },
+  {
+    link: "https://uaceit.com/",
+    img: "images/sponsors/uaceit.png"
+  },
   {
     link: "https://www.wolfram.com/language/",
     img: "images/sponsors/wolfram.png",
   },
 
-  { link: "https://www.jetbrains.com/", img: "images/sponsors/jetbrains.png" },
-
+  {
+    link: "https://www.jetbrains.com/",
+    img: "images/sponsors/jetbrains.png"
+  },
   {
     link: "https://www.hackathonsinternational.com/",
     img: "images/sponsors/hackathonsint.png",
   },
-
-  { link: "https://www.clerky.com/", img: "images/sponsors/clerky.jpg" },
-
-  { link: "https://www.voiceflow.com/", img: "images/sponsors/voiceflow.png" },
-
-  { link: "https://www.thesouledstore.com/", img: "images/sponsors/souled.jpg" },
-  { link: "https://www.outsystems.com/", img: "images/sponsors/outsystems.png" },
+  {
+    link: "https://www.clerky.com/",
+    img: "images/sponsors/clerky.jpg"
+  },
+  {
+    link: "https://www.voiceflow.com/",
+    img: "images/sponsors/voiceflow.png"
+  },
+  {
+    link: "https://www.thesouledstore.com/",
+    img: "images/sponsors/souled.jpg"
+  },
+  {
+    link: "https://www.outsystems.com/",
+    img: "images/sponsors/outsystems.png"
+  },  
 ]
 
 export default Sponsorlist
