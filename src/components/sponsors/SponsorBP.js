1271b4e22d1ebbc78be8a180730b686e0394b48a
Neeraj Patil <patilneeraj2212@gmail.com>
2022-01-18 23:44:57 +0530

    Sponsor fixed

--- src/components/sponsors/SponsorBP.js
+++ src/components/sponsors/SponsorBP.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react"
+import { Link } from "react-router-dom"
 
 const SponsorBP = (props) => {
-    return (
-        <Link to={{ pathname: props.add }} target="_blank" >
-            <div className='sponsor-bp' data-aos="fade-up">
-                <img src={props.link} alt="sponsor-logo" className="sponsor-logo" />
-            </div>
-        </Link >
-    )
+  return (
+    <Link to={{ pathname: props.add }} target="_blank">
+      <div className="sponsor-bp" data-aos="fade-up">
+        <img src={props.link} alt="sponsor-img" className="sponsor-img" />
+      </div>
+    </Link>
+  )
 }
 
 export default SponsorBP
