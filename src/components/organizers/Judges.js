834008ecf0b9703ec58e9f6b8a29fce82ed64f84
Pragati Priya <77202693+pragatipriya20@users.noreply.github.com>
2022-02-09 19:31:13 +0530

    updated judges section

--- src/components/organizers/Judges.js
+++ src/components/organizers/Judges.js
@@ -4,7 +4,7 @@ import './Organizers.css'
 // import img17 from '../../images/avatar.gif'
 import img1 from '../../images/judges/Abhinav.jpeg'
 import img2 from '../../images/judges/Akash.jpg'
-import img3 from '../../images/judges/Ameer Jhan.jpeg'
+import img3 from '../../images/judges/Ameer Jhan.jpg'
 import img4 from '../../images/judges/Dominic.png'
 import img5 from '../../images/judges/kaustubh.jpg'
 import img6 from '../../images/judges/ManishKumar.png'
@@ -12,43 +12,12 @@ import img7 from '../../images/judges/Sameer.png'
 import img8 from '../../images/judges/Shreya.jpeg'
 import img9 from '../../images/judges/Vinit.png'
 import img10 from '../../images/judges/MadhavBahl.jpeg'
+import img11 from '../../images/judges/Kaustubh.jpeg'
 
 
 const Judges = () => {
 
     const leads = [
-        {
-            name: 'Abhinav',
-            role: 'Software Development Engineer',
-            logo: `${img1}`,
-            linkedin: 'https://www.linkedin.com/in/mastersabh/',
-            twitter: '',
-            github: '',
-        },
-        {
-            name: 'Akash Bajpai',
-            role: 'Associate Software Engineer',
-            logo: `${img2}`,
-            linkedin: 'https://www.linkedin.com/in/akash4bajpai/',
-            twitter: '',
-            github: '',
-        },
-        {
-            name: 'Ameer Jhan',
-            role: 'Senior Software Development Engineer',
-            logo: `${img3}`,
-            linkedin: 'https://www.linkedin.com/in/ameerthehacker/',
-            twitter: '',
-            github: ''
-        },
-        {
-            name: 'Dominic Norton',
-            role: 'Product Manager',
-            logo: `${img4}`,
-            linkedin: 'https://www.linkedin.com/in/dominiconorton/',
-            twitter: '',
-            github: '',
-        },
         {
             name: 'Kaustubh Karkare',
             role: 'Senior Software Engineer / Tech Lead',
@@ -57,19 +26,20 @@ const Judges = () => {
             twitter: '',
             github: 'https://github.com/kaustubh-karkare'
         },
+        
         {
-            name: 'Manish Kumar Vats',
-            role: 'Android Developer',
-            logo: `${img6}`,
-            linkedin: 'https://www.linkedin.com/in/manish-kumar-vats',
-            twitter: ' ',
-            github: 'https://github.com/manish-Kumar-Vats/',
+            name: 'Vinit Shahdeo',
+            role: 'Software Engineer II',
+            logo: `${img9}`,
+            linkedin: 'https://www.linkedin.com/in/vinitshahdeo/',
+            twitter: '',
+            github: 'https://github.com/vinitshahdeo',
         },
         {
-            name: 'Kaustubh Maske Patil',
-            role: 'Co-Founder',
-            logo: `${img7}`,
-            linkedin: 'https://www.linkedin.com/in/kaustubh42/',
+            name: 'Abhinav',
+            role: 'Software Development Engineer',
+            logo: `${img1}`,
+            linkedin: 'https://www.linkedin.com/in/mastersabh/',
             twitter: '',
             github: '',
         },
@@ -82,12 +52,20 @@ const Judges = () => {
             github: ' ',
         },
         {
-            name: 'Vinit Shahdeo',
-            role: 'Software Engineer II',
-            logo: `${img9}`,
-            linkedin: 'https://www.linkedin.com/in/vinitshahdeo/',
+            name: 'Dominic Norton',
+            role: 'Product Manager',
+            logo: `${img4}`,
+            linkedin: 'https://www.linkedin.com/in/dominiconorton/',
             twitter: '',
-            github: 'https://github.com/vinitshahdeo',
+            github: '',
+        },
+        {
+            name: 'Ameer Jhan',
+            role: 'Senior Software Development Engineer',
+            logo: `${img3}`,
+            linkedin: 'https://www.linkedin.com/in/ameerthehacker/',
+            twitter: '',
+            github: 'https://github.com/ameerthehacker'
         },
         {
             name: 'Madhav Bahl',
@@ -95,7 +73,41 @@ const Judges = () => {
             logo: `${img10}`,
             linkedin: 'https://www.linkedin.com/in/madhavbahl/',
             twitter: '',
+            github: 'https://github.com/MadhavBahl',
+        },
+        {
+            name: 'Akash Bajpai',
+            role: 'Associate Software Engineer',
+            logo: `${img2}`,
+            linkedin: 'https://www.linkedin.com/in/akash4bajpai/',
+            twitter: '',
             github: '',
+        },
+        {
+            name: 'Manish Kumar Vats',
+            role: 'Android Developer',
+            logo: `${img6}`,
+            linkedin: 'https://www.linkedin.com/in/manish-kumar-vats',
+            twitter: ' ',
+            github: 'https://github.com/manish-Kumar-Vats/',
+        },
+        
+        {
+            name: 'Kaustubh Maske Patil',
+            role: 'Co-Founder',
+            logo: `${img11}`,
+            linkedin: 'https://www.linkedin.com/in/kaustubh42/',
+            twitter: '',
+            github: 'https://github.com/nikochiko',
+        },
+        
+        {
+            name: 'Sameer  Kashyap',
+            role: 'Software Engineer',
+            logo: `${img7}`,
+            linkedin: 'https://www.linkedin.com/in/sameer-kashyap-083a89184',
+            twitter: '',
+            github: 'https://github.com/Sameerkash',
         },
     ]
 
