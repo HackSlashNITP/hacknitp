4b3883467410f5b4aa4a59573ff08991e68b2e10
PARITOSH KUMAR <code.paritosh@outlook.com>
2022-02-11 18:23:45 +0530

    Update Prizes.js

--- src/components/prizes/Prizes.js
+++ src/components/prizes/Prizes.js
@@ -52,6 +52,7 @@ const Prizes = () => {
     "Certificate of Excellence",
   ]
   const girlsPrize = [
+    "100$ Digital Ocean Credits to all team members (100$ 60-day free trial + 125$ DO Credits).",
     "5 Year free Taskade Workspace upgrade to the team.",
     "1-month FREE access to all Business Plan features of the echo3D platform. ($89 each)",
     "INR 300/- GeeksForGeeks discount coupon to all team members.",
@@ -88,7 +89,6 @@ const Prizes = () => {
     "1-month access to all courses on Interview Cake.",
     "Voiceflow Professional License coupon to all team members (valued at $49 per month).",
     "30 day free-license to Wolfram | One for each team members.",
-    "All participants can download and use Sketch software for free during the hackathon.",
     "Certificate of Participation",
   ]
 
